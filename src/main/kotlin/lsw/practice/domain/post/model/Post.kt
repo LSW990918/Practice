bee0626f482b71ef59faf2b31d98bc9343b068a7
LSW990918 <leesywon99@gmail.com>
2024-02-02 15:57:11 +0900

    post 소프트 딜리트 구현

--- src/main/kotlin/lsw/practice/domain/post/model/Post.kt
+++ src/main/kotlin/lsw/practice/domain/post/model/Post.kt
@@ -2,9 +2,16 @@ package lsw.practice.domain.post.model
 
 import jakarta.persistence.*
 import lsw.practice.domain.user.model.User
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
 import java.time.LocalDateTime
 
 @Entity
+@SQLDelete(sql = "UPDATE post SET is_deleted = true WHERE id = ?") // DELETE 쿼리 날아올 시 대신 실행
+@Where(clause = "is_deleted = false")
+@OnDelete(action = OnDeleteAction.CASCADE)
 @Table(name = "post")
 class Post(
 
