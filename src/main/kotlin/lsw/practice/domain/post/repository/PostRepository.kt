7d6e53f1cab86886d4286d2184f0ff824b989e5f
LSW990918 <leesywon99@gmail.com>
2024-02-02 15:54:30 +0900

    post 패키지 서비스, 서비스임플, 컨트롤러 생성

---
+++ src/main/kotlin/lsw/practice/domain/post/repository/PostRepository.kt
@@ -0,0 +1,7 @@
+package lsw.practice.domain.post.repository
+
+import lsw.practice.domain.post.model.Post
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PostRepository: JpaRepository<Post, Long> {
+}
---
+++ src/main/kotlin/lsw/practice/domain/post/service/PostServiceImpl.kt
@@ -0,0 +1,89 @@
+package lsw.practice.domain.post.service
+
+import lsw.practice.domain.exception.InvalidCredentialException
+import lsw.practice.domain.exception.MismatchException
+import lsw.practice.domain.exception.ModelNotFoundException
+import lsw.practice.domain.post.dto.CreatePostRequest
+import lsw.practice.domain.post.dto.PostResponse
+import lsw.practice.domain.post.dto.UpdatePostRequest
+import lsw.practice.domain.post.model.Post
+import lsw.practice.domain.post.repository.PostRepository
+import lsw.practice.domain.user.repository.UserRepository
+import lsw.practice.infra.security.UserPrincipal
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class PostServiceImpl(
+    private val postRepository: PostRepository,
+    private val userRepository: UserRepository
+): PostService {
+    @Transactional
+    override fun createPost(
+        userPrincipal: UserPrincipal,
+        request: CreatePostRequest
+    ): PostResponse {
+        val user = userRepository.findByIdOrNull(userPrincipal.id)
+            ?: throw ModelNotFoundException("user", userPrincipal.id)
+        val post = Post(
+            title = request.title,
+            content = request.content,
+            name = user.name,
+            user = user
+        )
+        postRepository.save(post)
+        return post.toResponse()
+    }
+
+    @Transactional
+    override fun updatePost(
+        userPrincipal: UserPrincipal,
+        postId: Long,
+        request: UpdatePostRequest
+    ): PostResponse {
+        val post = postRepository.findByIdOrNull(postId)
+            ?: throw ModelNotFoundException("post", postId)
+        if (post.user.id != userPrincipal.id) {
+            throw MismatchException(userPrincipal.id, postId)
+        }
+        val (title, content) = request
+        post.title = title
+        post.content = content
+        return post.toResponse()
+    }
+
+    @Transactional
+    override fun deletePost(
+        userPrincipal: UserPrincipal,
+        postId: Long
+    ) {
+        val post = postRepository.findByIdOrNull(postId)
+            ?: throw ModelNotFoundException("post", postId)
+        if (post.user.id != userPrincipal.id) {
+            throw MismatchException(userPrincipal.id, postId)
+        }
+        postRepository.delete(post)
+    }
+
+    override fun getPost(postId: Long): PostResponse {
+        val post = postRepository.findByIdOrNull(postId)
+            ?: throw ModelNotFoundException("post", postId)
+        return post.toResponse()
+    }
+
+    override fun getPostList(): List<PostResponse> {
+        val postList = postRepository.findAll()
+        return postList.map { it.toResponse() }
+    }
+}
+
+fun Post.toResponse(): PostResponse {
+    return PostResponse(
+        id = id!!,
+        title = title,
+        content = content,
+        name = name,
+        createdAt = createdAt
+    )
+}
