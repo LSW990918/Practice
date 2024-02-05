adbf6db5f371978693a4a48efffd840ab2f6fb9e
LSW990918 <leesywon99@gmail.com>
2024-02-05 18:24:28 +0900

    post 리스폰스 시에 댓글 추가반환 하도록 쿼리 수정(fetchJoin)

--- src/main/kotlin/lsw/practice/domain/post/service/PostServiceImpl.kt
+++ src/main/kotlin/lsw/practice/domain/post/service/PostServiceImpl.kt
@@ -95,6 +95,7 @@ fun Post.toResponse(): PostResponse {
         title = title,
         content = content,
         name = name,
-        createdAt = createdAt
+        createdAt = createdAt,
+        comments = comments
     )
 }
